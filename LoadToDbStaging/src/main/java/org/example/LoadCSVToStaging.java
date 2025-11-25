283dcd4ddb25e1c99abbbb760ae214c31dc70640
Nam <22130173@st.hcmuaf.edu.vn>
2025-11-25 14:53:33 +0700

    comment following workflow

--- LoadToDbStaging/src/main/java/org/example/LoadCSVToStaging.java
+++ LoadToDbStaging/src/main/java/org/example/LoadCSVToStaging.java
@@ -33,7 +33,9 @@ public class LoadCSVToStaging {
         """;
 
         try (
+             //   3.5 Gọi DriverManager.getConnection() để bắt đầu kết nối đến database
                 Connection conn = DriverManager.getConnection(URL, USER, PASS);
+                //3.5.1 Khởi tạo PreparedStatement (SQL INSERT)
                 PreparedStatement ps = conn.prepareStatement(sql);
                 CSVReader reader = new CSVReader(new FileReader(CSV_FILE))
         ) {
@@ -47,7 +49,7 @@ public class LoadCSVToStaging {
 
                 try {
                     System.out.println("Loading: " + Arrays.toString(row));
-
+                // 3.6 Thực hiện: Cắt chuỗi (Truncate): Kiểm tra độ dài name, link, specs... (nếu quá dài thì cắt)
                     ps.setString(1, truncate(row[0], 255));   // name
                     ps.setString(2, truncate(row[1], 50));    // screenSize
                     ps.setString(3, truncate(row[2], 100));   // screenTechnology
@@ -63,7 +65,7 @@ public class LoadCSVToStaging {
 
                     ps.setString(10, truncate(row[9], 50));   // version (giới hạn 50 ký tự)
                     ps.setString(11, truncate(row[10], 50));  // color
-
+                    // Làm sạch số: Dùng Regex replaceAll("\\D", "") cho price, battery
                     // price
                     if (row[11] == null || row[11].isEmpty()) ps.setNull(12, Types.DECIMAL);
                     else ps.setBigDecimal(12, new java.math.BigDecimal(row[11].replaceAll("\\D", "")));
@@ -84,17 +86,18 @@ public class LoadCSVToStaging {
 
                     ps.setString(17, truncate(row[16], 10));  // nfc
                     ps.setString(18, truncate(row[17], 50));  // releaseDate
-
+                    //3.7 Thực thi ps.executeUpdate() để lưu dữ liệu xuống Database.
                     ps.executeUpdate();
                     successCount++;
 
                 } catch (Exception rowError) {
+                    //3.7.1 Ghi Log "Lỗi dòng: [Data]"
                     errorCount++;
                     System.err.println("Lỗi dòng: " + Arrays.toString(row));
                     System.err.println("Chi tiết: " + rowError.getMessage());
                 }
             }
-
+             // 3.8 In ra màn hình số dòng lỗi, thành công
             System.out.println("\nDONE: Load thành công " + successCount + " dòng");
             if (errorCount > 0) {
                 System.out.println("Có " + errorCount + " dòng lỗi bị bỏ qua");
@@ -105,13 +108,13 @@ public class LoadCSVToStaging {
         }
     }
 
-    // Hàm cắt chuỗi để tránh lỗi Data too long
+    //Ham truncate để cắt chuỗi nếu vượt quá độ dài tối đa
     private static String truncate(String value, int maxLength) {
         if (value == null || value.isEmpty()) return value;
         return value.length() > maxLength ? value.substring(0, maxLength) : value;
     }
 
-    // Hàm đọc cấu hình từ file variable.csv
+    // 3.4 Hàm loadConfig() khởi tạo đối tượng CSVReader để đọc nội dung file Variable.csv và Nạp biến db.url, db.user, db.pass
     private static void loadConfig(String configFilePath) {
         try (CSVReader reader = new CSVReader(new FileReader(configFilePath))) {
             String[] row;
@@ -149,7 +152,6 @@ public class LoadCSVToStaging {
     }
     private static String chooseVariableCsv() {
         try {
-            // Dùng look and feel của hệ điều hành
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception ignored) {}
 
